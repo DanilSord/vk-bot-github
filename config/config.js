9650f8d14a9d79cacc60d99ceef3303e7510679f
DanilSord <danil_sword@mail.ru>
2020-05-19 14:36:36 +0700

    open config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: process.env.PORT,
+    CONFIRMATION: process.env.CONFIRMATION,
+    mongoURI: process.env.mongoURI,
+    TOKEN:process.env.TOKEN,
+    SENDMETHOD: process.env.SENDMETHOD,
+    APIVERSION: process.env.APIVERSION
+  };
