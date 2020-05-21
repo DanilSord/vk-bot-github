8f0b5c7be449655ba3008ad4e5bf9f6a169fb815
DanilSord <danil_sword@mail.ru>
2020-05-21 10:27:32 +0700

    Increase data size in body-parser to 50mb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const connectDB = require('./models/connectDB.js');
 const { PORT } = require('./config/config.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}))
 setRoutes(app);
 connectDB();
 
